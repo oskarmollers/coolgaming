a09d7dfc8998aee44bccb4bbb3e2118d27b72bda
Elev <Elev@TEB127022>
2020-11-02 09:08:02 +0100

    La till mer

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,6 +11,8 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
+            Console.WriteLine("Teknikum, Växjö");
+            Console.WriteLine("Oskar");
         }
     }
 }
